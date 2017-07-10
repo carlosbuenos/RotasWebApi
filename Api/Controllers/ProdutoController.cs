45819a9b84b21aeb82184d281007992687562c40
victor <victorwork@yahoo.com.br>
2017-07-10 13:30:20 -0300

    primeiro commit.

---
+++ Api/Controllers/ProdutoController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using System.Linq;
+
+namespace Api.Controllers
+{
+    [RoutePrefix("api/produto")]
+    public class ProdutoController : ApiController
+    { 
+        [HttpGet]
+        public List<string> BuscarTudo()
+        {
+            return new List<string>
+            {
+                "mouse de bolinha",
+                "teclado sem fio",
+                "monitor lcd 19",
+                "monitor led 21"
+            };
+        }
+
+        [HttpGet, Route("{nome}")]
+        public List<string> BuscarPorNome(string nome)
+        {
+            return new List<string>
+            {
+                "mouse de bolinha",
+                "teclado sem fio",
+                "monitor lcd 19",
+                "monitor led 21"
+            }.Where(x => x.Trim().ToLower().Contains(nome.Trim().ToLower())).ToList();
+        }
+    }
+}
